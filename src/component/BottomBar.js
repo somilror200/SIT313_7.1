81b9be177e005dbeb749c2263202c9e0069f2d79
Somil Garak <somilror200@gmail.com>
2023-10-08 13:56:47 +1100

    initial commit

--- src/component/BottomBar.js
+++ src/component/BottomBar.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import './BottomBar.css';
+import emailjs from "emailjs-com";
 
 function BottomBar() {
+  function sendEmail(e) {
+    e.preventDefault();
+
+    emailjs.sendForm('service_wcub1rh', 'template_y95uzac', e.target, 'bUAXeIgJbhSWgtXuP')
+      .then((result) => {
+        console.log(result.text);
+      }, (error) => {
+        console.log(error.text);
+      });
+      e.target.reset()
+  };
+
   return (
-    <div className="bottom-bar">
+    <form className="bottom-bar" onSubmit={sendEmail}>
       <p>Signup for Daily Insider:</p>
       <div className="subscribe-box">
-        <input type="email" placeholder="Enter your email" />
-        <button>Subscribe</button>
+        <input
+          type="email"
+          name="email"
+          placeholder="Enter your email"
+        />
+        <button type="submit">Subscribe</button>
       </div>
-    </div>
+    </form>
   );
 }
 
